52ba6c8d70eeea164577fe097d4c0b3b1d3db848
takafan <takafan@163.com>
2018-02-19 21:26:13 +0800

    a

--- settings.js
+++ settings.js
@@ -1,7 +1,7 @@
 let host = 'http://192.168.1.59'
 
 if (process.env.NODE_ENV === 'production') {
-  host = ''
+  host = '' // nginx conf add line: proxy_set_header Host $host;
 }
 
 console.log('host: ' + host)
