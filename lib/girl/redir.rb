109b38bfe323bef5fde160fa36d2633a311f2df4
takafan <takafan@163.com>
2018-07-24 00:09:55 +0800

    no thread

--- lib/girl/redir.rb
+++ lib/girl/redir.rb
@@ -5,14 +5,13 @@ module Girl
   class Redir
 
     def initialize(port, relay_host, relay_port, hex_block = nil)
-      @relay_sockaddr = Socket.sockaddr_in(relay_port, relay_host)
+      relay_sockaddr = Socket.sockaddr_in(relay_port, relay_host)
 
       if hex_block
         Girl::Hex.class_eval(hex_block)
       end
 
-      @hex = Girl::Hex.new
-
+      hex = Girl::Hex.new
       redir = Socket.new(Socket::AF_INET, Socket::SOCK_STREAM, 0)
       redir.setsockopt(Socket::SOL_TCP, Socket::TCP_NODELAY, 1)
       redir.setsockopt(Socket::SOL_SOCKET, Socket::SO_REUSEADDR, 1)
@@ -21,149 +20,158 @@ module Girl
       redir.listen(128)
       puts "#{Process.pid} Listening on #{port}"
 
-      @thrs = []
+      reads = {
+        redir => {
+          role: :redir
+        }
+      }
+      buffs = {}
+      writes = {}
 
       loop do
-        rs, ws = IO.select([ redir ])
-        select(*rs.first.accept_nonblock)
-      end
-    end
+        rs, ws = IO.select(reads.keys, writes.keys)
 
-    private
-
-    def select(client, info)
-      @thrs << Thread.new do
-        print "#{Process.pid} t#{@thrs.size} #{info.ip_unpack.join(':')} #{Time.new} "
-
-        reads = {
-          client => {
-            role: :source,
-            twin: nil
-          }
-        }
-
-        buffs = {
-          client => ''
-        }
-
-        writes = {}
-
-        loop do
-          rs, ws = IO.select(reads.keys, writes.keys)
-
-          ws.each do |sock|
-            if sock.closed?
-              next
-            end
-
-            buff = buffs[sock]
-
-            begin
-              written = sock.write_nonblock(buff)
-            rescue IO::WaitWritable
-              print ' w'
-              next
-            rescue IOError, Errno::ECONNRESET, Errno::EFAULT, Errno::ETIMEDOUT, Errno::EINVAL, Errno::EPIPE, Errno::ECONNREFUSED, Errno::EHOSTUNREACH, Errno::ENETUNREACH, Errno::ENOENT => e
-              puts "write #{writes[sock][:role]} #{e.message}"
-              close(sock, writes[sock][:twin], false)
-            end
-
-            buffs[sock] = buff[written..-1]
-            unless buffs[sock].empty?
-              next
-            end
-
-            writes.delete(sock)
+        rs.each do |sock|
+          if sock.closed?
+            puts 'rs already closed?'
+            next
           end
 
-          rs.each do |sock|
-            if sock.closed?
+          case reads[sock][:role]
+          when :redir
+            source, addr = sock.accept_nonblock
+            print "p#{Process.pid} #{Time.new} "
+
+            reads[source] = {
+              role: :source,
+              twin: nil
+            }
+            buffs[source] = ''
+          when :source
+            begin
+              data = sock.read_nonblock(4096)
+            rescue EOFError => e
+              deal_exception(e, sock, reads, buffs, writes)
+              next
+            rescue IOError, Errno::ECONNRESET, Errno::EFAULT, Errno::ETIMEDOUT, Errno::EINVAL, Errno::EPIPE, Errno::ECONNREFUSED, Errno::EHOSTUNREACH, Errno::ENETUNREACH, Errno::ENOENT => e
+              puts "read source #{e.message}"
+              deal_exception(e, sock, reads, buffs, writes)
               next
             end
 
-            case reads[sock][:role]
-            when :source
-              relay = reads[sock][:twin]
+            twin = reads[sock][:twin]
+
+            unless twin
+              begin
+                dst_addr = sock.getsockopt(Socket::SOL_IP, 80) # SO_ORIGINAL_DST https://github.com/torvalds/linux/blob/master/include/uapi/linux/netfilter_ipv4.h
+              rescue Errno::ENOENT => e
+                puts "get SO_ORIGINAL_DST #{e.message}"
+                deal_exception(e, sock, reads, buffs, writes)
+                next
+              end
+
+              dst_family, dst_port, dst_host = dst_addr.unpack("nnN")
+              data = hex.mix(data, dst_host, dst_port)
+              twin = Socket.new(Socket::AF_INET, Socket::SOCK_STREAM, 0)
+              twin.setsockopt(Socket::SOL_TCP, Socket::TCP_NODELAY, 1)
 
               begin
-                data = sock.read_nonblock(4096)
-              rescue EOFError => e
-                close(sock, relay)
-              rescue IOError, Errno::ECONNRESET, Errno::EFAULT, Errno::ETIMEDOUT, Errno::EINVAL, Errno::EPIPE, Errno::ECONNREFUSED, Errno::EHOSTUNREACH, Errno::ENETUNREACH, Errno::ENOENT => e
-                puts "read source #{e.message}"
-                close(sock, relay, false)
+                twin.connect_nonblock(relay_sockaddr)
+              rescue IO::WaitWritable
+                reads[sock][:twin] = twin
+                reads[twin] = {
+                  role: :relay,
+                  twin: sock
+                }
+                buffs[twin] = ''
+              rescue Errno::EISCONN => e
+                puts "already connected?"
+              rescue Errno::EFAULT, Errno::ETIMEDOUT, Errno::EHOSTUNREACH, Errno::ENETUNREACH, Errno::ENOENT => e
+                deal_exception(e, twin, reads, buffs, writes)
+                next
               end
-
-              unless relay
-                begin
-                  dst_addr = sock.getsockopt(Socket::SOL_IP, 80) # SO_ORIGINAL_DST https://github.com/torvalds/linux/blob/master/include/uapi/linux/netfilter_ipv4.h
-                rescue Errno::ENOENT => e
-                  puts "get SO_ORIGINAL_DST #{e.message}"
-                  close(sock, relay, false)
-                end
-
-                dst_family, dst_port, dst_host = dst_addr.unpack("nnN")
-                data = @hex.mix(data, dst_host, dst_port)
-
-                relay = Socket.new(Socket::AF_INET, Socket::SOCK_STREAM, 0)
-                relay.setsockopt(Socket::SOL_TCP, Socket::TCP_NODELAY, 1)
-
-                begin
-                  relay.connect_nonblock(@relay_sockaddr)
-                rescue IO::WaitWritable
-                  reads[sock][:twin] = relay
-                  reads[relay] = {
-                    role: :relay,
-                    twin: sock
-                  }
-
-                  buffs[relay] = ''
-                rescue Errno::EISCONN => e
-                  puts "time cross?"
-                rescue Errno::EFAULT, Errno::ETIMEDOUT, Errno::EHOSTUNREACH, Errno::ENETUNREACH, Errno::ENOENT => e
-                  puts "connecting relay #{e.message}"
-                  close(relay, sock, false)
-                end
-              end
-
-              buffs[relay] << @hex.swap(data)
-              writes[relay] = {
-                role: :relay,
-                twin: sock
-              }
-            when :relay
-              source = reads[sock][:twin]
-
-              begin
-                data = sock.read_nonblock(4096)
-              rescue EOFError => e
-                close(sock, source)
-              rescue IOError, Errno::ECONNRESET, Errno::EFAULT, Errno::ETIMEDOUT, Errno::EINVAL, Errno::EPIPE, Errno::ECONNREFUSED, Errno::EHOSTUNREACH, Errno::ENETUNREACH, Errno::ENOENT => e
-                puts "read relay #{e.message}"
-                close(sock, source, false)
-              end
-
-              buffs[source] << @hex.swap(data)
-              writes[source] = {
-                role: :source,
-                twin: sock
-              }
             end
+
+            buffs[twin] << hex.swap(data)
+            writes[twin] = {
+              role: :relay
+            }
+          when :relay
+            begin
+              data = sock.read_nonblock(4096)
+            rescue EOFError => e
+              deal_exception(e, sock, reads, buffs, writes)
+              next
+            rescue IOError, Errno::ECONNRESET, Errno::EFAULT, Errno::ETIMEDOUT, Errno::EINVAL, Errno::EPIPE, Errno::ECONNREFUSED, Errno::EHOSTUNREACH, Errno::ENETUNREACH, Errno::ENOENT => e
+              puts "read relay #{e.message}"
+              deal_exception(e, sock, reads, buffs, writes)
+              next
+            end
+
+            twin = reads[sock][:twin]
+            buffs[twin] << hex.swap(data)
+            writes[twin] = {
+              role: :source
+            }
+          end
+        end
+
+        ws.each do |sock|
+          if sock.closed?
+            puts 'ws already closed?'
+            next
+          end
+
+          buff = buffs[sock]
+
+          begin
+            written = sock.write_nonblock(buff)
+          rescue IO::WaitWritable
+            print ' w'
+            next
+          rescue IOError, Errno::ECONNRESET, Errno::EFAULT, Errno::ETIMEDOUT, Errno::EINVAL, Errno::EPIPE, Errno::ECONNREFUSED, Errno::EHOSTUNREACH, Errno::ENETUNREACH, Errno::ENOENT => e
+            puts "write #{writes[sock][:role]} #{e.message}"
+            deal_exception(e, sock, reads, buffs, writes)
+            next
+          end
+
+          buffs[sock] = buff[written..-1]
+          unless buffs[sock].empty?
+            print ' .'
+            next
+          end
+
+          e = writes.delete(sock)[:twin_exception]
+          if e
+            sock.setsockopt(Socket::SOL_SOCKET, Socket::SO_LINGER, [1, 0].pack("ii")) unless e.is_a?(EOFError)
+            sock.close
+            reads.delete(sock)
+            buffs.delete(sock)
           end
         end
       end
     end
 
-    def close(sock, twin, is_eof = true)
-      sock.setsockopt(Socket::Option.linger(true, 0)) unless is_eof;
+    private
+
+    def deal_exception(e, sock, reads, buffs, writes)
+      sock.setsockopt(Socket::SOL_SOCKET, Socket::SO_LINGER, [1, 0].pack("ii")) unless e.is_a?(EOFError)
       sock.close
+      twin = reads.delete(sock).delete(:twin)
+      buffs.delete(sock)
+      writes.delete(sock)
 
       if twin && !twin.closed?
-        twin.setsockopt(Socket::Option.linger(true, 0)) unless is_eof;
-        twin.close
+        if writes.include?(twin)
+          writes[twin][:twin_exception] = e
+        else
+          twin.setsockopt(Socket::SOL_SOCKET, Socket::SO_LINGER, [1, 0].pack("ii")) unless e.is_a?(EOFError)
+          twin.close
+          reads.delete(twin)
+          buffs.delete(twin)
+          writes.delete(twin)
+        end
       end
-
-      @thrs.delete(Thread.current).exit
     end
 
   end
--- lib/girl/relay.rb
+++ lib/girl/relay.rb
@@ -9,8 +9,7 @@ module Girl
         Girl::Xeh.class_eval(xeh_block)
       end
 
-      @xeh = Girl::Xeh.new
-
+      xeh = Girl::Xeh.new
       relay = Socket.new(Socket::AF_INET, Socket::SOCK_STREAM, 0)
       relay.setsockopt(Socket::SOL_TCP, Socket::TCP_NODELAY, 1)
       relay.setsockopt(Socket::SOL_SOCKET, Socket::SO_REUSEADDR, 1)
@@ -19,150 +18,162 @@ module Girl
       relay.listen(128) # cat /proc/sys/net/ipv4/tcp_max_syn_backlog
       puts "#{Process.pid} Listening on #{port}"
 
-      @thrs = []
+      reads = {
+        relay => {
+          role: :relay
+        }
+      }
+      buffs = {}
+      writes = {}
 
       loop do
-        rs, ws = IO.select([ relay ])
-        select(*rs.first.accept_nonblock)
-      end
-    end
+        rs, ws = IO.select(reads.keys, writes.keys)
 
-    private
-
-    def select(client, info)
-      @thrs << Thread.new do
-        puts "#{Process.pid} t#{@thrs.size} #{info.ip_unpack.join(':')} #{Time.new}"
-
-        reads = {
-          client => {
-            role: :redir,
-            twin: nil
-          }
-        }
-
-        buffs = {
-          client => ''
-        }
-
-        writes = {}
-
-        loop do
-          rs, ws = IO.select(reads.keys, writes.keys)
-
-          # write firstly
-          ws.each do |sock|
-            if sock.closed?
-              # already closed by twin's error
-              next
-            end
-
-            buff = buffs[sock]
-
-            begin
-              written = sock.write_nonblock(buff)
-            rescue IO::WaitWritable
-              print ' w'
-              next
-            rescue IOError, Errno::ECONNRESET, Errno::EFAULT, Errno::ETIMEDOUT, Errno::EINVAL, Errno::EPIPE, Errno::ECONNREFUSED, Errno::EHOSTUNREACH, Errno::ENETUNREACH, Errno::ENOENT => e
-              puts "write #{writes[sock][:role]} #{e.message}"
-              close(sock, writes[sock][:twin], false)
-            end
-
-            buffs[sock] = buff[written..-1]
-            unless buffs[sock].empty?
-              next
-            end
-
-            writes.delete(sock)
+        rs.each do |sock|
+          if sock.closed?
+            puts 'rs already closed?'
+            next
           end
 
-          # read secondly, happy eof
-          rs.each do |sock|
-            if sock.closed?
-              # already closed by write error, or twin's error
+          case reads[sock][:role]
+          when :relay
+            redir, addr = sock.accept_nonblock
+            print "p#{Process.pid} #{Time.new} "
+
+            reads[redir] = {
+              role: :redir,
+              addr: addr,
+              twin: nil
+            }
+            buffs[redir] = ''
+          when :redir
+            begin
+              data = xeh.swap(sock.read_nonblock(4096))
+            rescue EOFError => e
+              deal_exception(e, sock, reads, buffs, writes)
+              next
+            rescue IOError, Errno::ECONNRESET, Errno::EFAULT, Errno::ETIMEDOUT, Errno::EINVAL, Errno::EPIPE, Errno::ECONNREFUSED, Errno::EHOSTUNREACH, Errno::ENETUNREACH, Errno::ENOENT => e
+              puts "read redir #{e.message}"
+              deal_exception(e, sock, reads, buffs, writes)
               next
             end
 
-            case reads[sock][:role]
-            when :redir
-              dest = reads[sock][:twin]
+            twin = reads[sock][:twin]
+
+            unless twin
+              ret = xeh.decode(data, reads[sock][:addr])
+              unless ret[:success]
+                puts ret[:error]
+                sock.setsockopt(Socket::SOL_SOCKET, Socket::SO_LINGER, [1, 0].pack("ii"))
+                sock.close
+                reads.delete(sock)
+                buffs.delete(sock)
+              end
+
+              data, dst_host, dst_port = ret[:data]
+              twin = Socket.new(Socket::AF_INET, Socket::SOCK_STREAM, 0)
+              twin.setsockopt(Socket::SOL_TCP, Socket::TCP_NODELAY, 1)
 
               begin
-                data = @xeh.swap(sock.read_nonblock(4096))
-              rescue EOFError => e
-                close(sock, dest)
-              rescue IOError, Errno::ECONNRESET, Errno::EFAULT, Errno::ETIMEDOUT, Errno::EINVAL, Errno::EPIPE, Errno::ECONNREFUSED, Errno::EHOSTUNREACH, Errno::ENETUNREACH, Errno::ENOENT => e
-                puts "read redir #{e.message}"
-                close(sock, dest, false)
+                twin.connect_nonblock(Socket.sockaddr_in(dst_port, dst_host))
+              rescue IO::WaitWritable
+                reads[sock][:twin] = twin
+                reads[twin] = {
+                  role: :dest,
+                  twin: sock
+                }
+
+                buffs[twin] = ''
+              rescue Errno::EISCONN => e
+                puts "time cross?"
+              rescue Errno::EFAULT, Errno::ETIMEDOUT, Errno::EHOSTUNREACH, Errno::ENETUNREACH, Errno::ENOENT => e
+                puts "connecting dest #{e.message}"
+                deal_exception(e, twin, reads, buffs, writes)
+                next
               end
-
-              unless dest
-                ret = @xeh.decode(data, info)
-                unless ret[:success]
-                  puts ret[:error]
-                  close(sock, false)
-                end
-
-                data, dst_host, dst_port = ret[:data]
-                dest = Socket.new(Socket::AF_INET, Socket::SOCK_STREAM, 0)
-                dest.setsockopt(Socket::SOL_TCP, Socket::TCP_NODELAY, 1)
-
-                begin
-                  dest.connect_nonblock(Socket.sockaddr_in(dst_port, dst_host))
-                rescue IO::WaitWritable
-                  reads[sock][:twin] = dest
-                  reads[dest] = {
-                    role: :dest,
-                    twin: sock
-                  }
-
-                  buffs[dest] = ''
-                rescue Errno::EISCONN => e
-                  puts "time cross?"
-                rescue Errno::EFAULT, Errno::ETIMEDOUT, Errno::EHOSTUNREACH, Errno::ENETUNREACH, Errno::ENOENT => e
-                  puts "connecting dest #{e.message}"
-                  close(dest, sock, false)
-                end
-              end
-
-              buffs[dest] << data
-              writes[dest] = {
-                role: :dest,
-                twin: sock
-              }
-            when :dest
-              redir = reads[sock][:twin]
-
-              begin
-                data = sock.read_nonblock(4096)
-              rescue EOFError => e
-                close(sock, redir)
-              rescue IOError, Errno::ECONNRESET, Errno::EFAULT, Errno::ETIMEDOUT, Errno::EINVAL, Errno::EPIPE, Errno::ECONNREFUSED, Errno::EHOSTUNREACH, Errno::ENETUNREACH, Errno::ENOENT => e
-                puts "read dest #{e.message}"
-                close(sock, redir, false)
-              end
-
-              buffs[redir] << @xeh.swap(data)
-              writes[redir] = {
-                role: :redir,
-                twin: sock
-              }
             end
+
+            buffs[twin] << data
+            writes[twin] = {
+              role: :dest
+            }
+          when :dest
+            begin
+              data = sock.read_nonblock(4096)
+            rescue EOFError => e
+              deal_exception(e, sock, reads, buffs, writes)
+              next
+            rescue IOError, Errno::ECONNRESET, Errno::EFAULT, Errno::ETIMEDOUT, Errno::EINVAL, Errno::EPIPE, Errno::ECONNREFUSED, Errno::EHOSTUNREACH, Errno::ENETUNREACH, Errno::ENOENT => e
+              puts "read dest #{e.message}"
+              deal_exception(e, sock, reads, buffs, writes)
+              next
+            end
+
+            twin = reads[sock][:twin]
+
+            buffs[twin] << xeh.swap(data)
+            writes[twin] = {
+              role: :redir
+            }
+          end
+        end
+
+        ws.each do |sock|
+          if sock.closed?
+            puts 'ws already closed?'
+            next
+          end
+
+          buff = buffs[sock]
+
+          begin
+            written = sock.write_nonblock(buff)
+          rescue IO::WaitWritable
+            print ' w'
+            next
+          rescue IOError, Errno::ECONNRESET, Errno::EFAULT, Errno::ETIMEDOUT, Errno::EINVAL, Errno::EPIPE, Errno::ECONNREFUSED, Errno::EHOSTUNREACH, Errno::ENETUNREACH, Errno::ENOENT => e
+            puts "write #{writes[sock][:role]} #{e.message}"
+            deal_exception(e, sock, reads, buffs, writes)
+            next
+          end
+
+          buffs[sock] = buff[written..-1]
+          unless buffs[sock].empty?
+            print ' .'
+            next
+          end
+
+          e = writes.delete(sock)[:twin_exception]
+          if e
+            sock.setsockopt(Socket::SOL_SOCKET, Socket::SO_LINGER, [1, 0].pack("ii")) unless e.is_a?(EOFError)
+            sock.close
+            reads.delete(sock)
+            buffs.delete(sock)
           end
         end
       end
     end
 
-    def close(sock, twin, is_eof = true)
-      sock.setsockopt(Socket::Option.linger(true, 0)) unless is_eof;
+    private
+
+    def deal_exception(e, sock, reads, buffs, writes)
+      sock.setsockopt(Socket::SOL_SOCKET, Socket::SO_LINGER, [1, 0].pack("ii")) unless e.is_a?(EOFError)
       sock.close
+      twin = reads.delete(sock).delete(:twin)
+      buffs.delete(sock)
+      writes.delete(sock)
 
       if twin && !twin.closed?
-        twin.setsockopt(Socket::Option.linger(true, 0)) unless is_eof;
-        twin.close
+        if writes.include?(twin)
+          writes[twin][:twin_exception] = e
+        else
+          twin.setsockopt(Socket::SOL_SOCKET, Socket::SO_LINGER, [1, 0].pack("ii")) unless e.is_a?(EOFError)
+          twin.close
+          reads.delete(twin)
+          buffs.delete(twin)
+          writes.delete(twin)
+        end
       end
-
-      @thrs.delete(Thread.current).exit
     end
 
   end
