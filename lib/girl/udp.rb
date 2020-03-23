3b6be2c95c9bfd204f4d0b6ab722ced7ec96369c
takafan <takafan@163.com>
2020-03-24 00:01:35 +0800

    add udp

---
+++ lib/girl/udp.rb
@@ -0,0 +1,184 @@
+require 'girl/hex'
+require 'girl/version'
+require 'socket'
+
+##
+# Girl::Udp - 转发udp。近端。
+#
+# usage
+# =====
+#
+# Girl::Udpd.new( 3030 ).looping # 远端
+#
+# Girl::Udp.new( 'your.server.ip', 3030, 1313 ).looping # 近端
+#
+# apt-get install xtables-addons-dkms
+# echo 'nf_tproxy_ipv4' > /etc/modules-load.d/nf_tproxy_ipv4.conf
+#
+# iptables -t mangle -I PREROUTING -p udp -d game.server.ip -j TPROXY --tproxy-mark 0x1/0x1 --on-port 1313
+#
+# ip rule add fwmark 1 lookup 100
+# ip route add local 0.0.0.0/0 dev lo table 100
+#
+module Girl
+  class Udp
+
+    def initialize( udpd_host, udpd_port = 3030, redir_port = 1313 )
+      @hex = Girl::Hex.new
+      @mutex = Mutex.new
+      @reads = []
+      @writes = []
+      @roles = {} # sock => :ctlr / :redir / :udp
+      @infos = {} # redir => {}
+
+      redir = Socket.new( Socket::AF_INET, Socket::SOCK_DGRAM, 0 )
+      redir.setsockopt( Socket::SOL_SOCKET, Socket::SO_REUSEPORT, 1 )
+      redir.setsockopt( Socket::SOL_IP, 19, 1 )
+      redir.setsockopt( Socket::SOL_IP, 20, 1 )
+      redir.bind( Socket.sockaddr_in( redir_port, '0.0.0.0' ) )
+      puts "redir bound on #{ redir_port } #{ Time.new }"
+
+      redir_info = {
+        udpd_addr: Socket.sockaddr_in( udpd_port, udpd_host ),
+        src_addrs: {}, # src_addr => udp
+        udp_ids: {},   # udp_id => udp
+        udps: {}       # udp => {
+                       #   src_addr: src_addr,
+                       #   last_recv_at: now
+                       # }
+      }
+
+      @redir = redir
+      @redir_info = redir_info
+      @roles[ redir ] = :redir
+      @infos[ redir ] = redir_info
+      @reads << redir
+
+      ctlr, ctlw = IO.pipe
+      @ctlw = ctlw
+      @roles[ ctlr ] = :ctlr
+      @reads << ctlr
+    end
+
+    def looping
+      loop_expire
+
+      loop do
+        rs, ws = IO.select( @reads, @writes )
+
+        @mutex.synchronize do
+          rs.each do | sock |
+            case @roles[ sock ]
+            when :ctlr
+              read_ctlr( sock )
+            when :redir
+              read_redir( sock )
+            when :udp
+              read_udp( sock )
+            end
+          end
+
+          ws.each do | sock |
+            close_udp( sock )
+          end
+        end
+      end
+    end
+
+    def quit!
+      exit
+    end
+
+    private
+
+    def read_ctlr( ctlr )
+      udp_id = ctlr.read( 8 ).unpack( 'Q>' ).first
+      udp = @redir_info[ :udp_ids ][ udp_id ]
+
+      if udp
+        # puts "debug expire #{ @roles[ udp ] } #{ udp_id } #{ Time.new }"
+
+        unless @writes.include?( udp )
+          @writes << udp
+        end
+      end
+    end
+
+    def read_redir( redir )
+      data, addrinfo, rflags, *controls = redir.recvmsg
+
+      # puts "debug #{ data.inspect } #{ Time.new }"
+      # puts "debug #{ controls.inspect } #{ Time.new }"
+
+      now = Time.new
+      info = @infos[ redir ]
+      src_addr = addrinfo.to_sockaddr
+      udp = info[ :src_addrs ][ src_addr ]
+
+      unless udp
+        udp = Socket.new( Socket::AF_INET, Socket::SOCK_DGRAM, 0 )
+        udp.setsockopt( Socket::SOL_SOCKET, Socket::SO_REUSEPORT, 1 )
+        udp.bind( Socket.sockaddr_in( 0, '0.0.0.0' ) )
+
+        # puts "debug a new udp bound on #{ udp.local_address.ip_unpack.last } #{ Time.new }"
+
+        udp_id = @hex.gen_random_num
+        info[ :src_addrs ][ src_addr ] = udp
+        info[ :udp_ids ][ udp_id ] = udp
+        info[ :udps ][ udp ] = {
+          udp_id: udp_id,
+          src_addr: src_addr,
+          last_recv_at: now
+        }
+
+        @roles[ udp ] = :udp
+        @reads << udp
+
+        ancdata = controls.find { | _ancdata | _ancdata.cmsg_is?( Socket::SOL_IP, 20 ) }
+        dest_addr = ancdata.data
+        udp.sendmsg( dest_addr, 0, info[ :udpd_addr ] )
+      end
+
+      udp.sendmsg( data, 0, info[ :udpd_addr ] )
+    end
+
+    def read_udp( udp )
+      data, addrinfo, rflags, *controls = udp.recvmsg
+
+      udp_info = @redir_info[ :udps ][ udp ]
+      return unless udp_info
+
+      udp_info[ :last_recv_at ] = Time.new
+      @redir.sendmsg( data, 0, udp_info[ :src_addr ] )
+    end
+
+    def close_udp( udp )
+      udp.close
+      @reads.delete( udp )
+      @writes.delete( udp )
+      @roles.delete( udp )
+      udp_info = @redir_info[ :udps ].delete( udp )
+      @redir_info[ :src_addrs ].delete( udp_info[ :src_addr ] )
+      @redir_info[ :udp_ids ].delete( udp_info[ :udp_id ] )
+    end
+
+    def loop_expire
+      Thread.new do
+        loop do
+          sleep 60
+
+          @mutex.synchronize do
+            now = Time.new
+
+            @redir_info[ :udps ].values.each do | udp_info |
+              if now - udp_info[ :last_recv_at ] > 3600
+                @ctlw.write( udp_info[ :udp_id ].pack( 'Q>' ) )
+              end
+            end
+          end
+        end
+      end
+    end
+
+  end
+end
---
+++ lib/girl/udpd.rb
@@ -0,0 +1,166 @@
+require 'girl/hex'
+require 'girl/version'
+require 'socket'
+
+##
+# Girl::Udpd - 转发udp。远端。
+#
+module Girl
+  class Udpd
+
+    def initialize( port = 3030 )
+      @hex = Girl::Hex.new
+      @mutex = Mutex.new
+      @reads = []
+      @writes = []
+      @roles = {} # sock => :ctlr / :udpd / :dest
+      @infos = {} # udpd => {}
+
+      udpd = Socket.new( Socket::AF_INET, Socket::SOCK_DGRAM, 0 )
+      udpd.setsockopt( Socket::SOL_SOCKET, Socket::SO_REUSEPORT, 1 )
+      udpd.bind( Socket.sockaddr_in( port, '0.0.0.0' ) )
+      puts "udpd bound on #{ port } #{ Time.new }"
+
+      udpd_info = {
+        udp_addrs: {}, # udp_addr => dest
+        dest_ids: {},  # dest_id => dest
+        dests: {}      # dest => {
+                       #   dest_id: dest_id,
+                       #   udp_addr: udp_addr,
+                       #   dest_addr: dest_addr,
+                       #   last_recv_at: now
+                       # }
+      }
+
+      @udpd = udpd
+      @udpd_info = udpd_info
+      @roles[ udpd ] = :udpd
+      @infos[ udpd ] = udpd_info
+      @reads << udpd
+
+      ctlr, ctlw = IO.pipe
+      @ctlw = ctlw
+      @roles[ ctlr ] = :ctlr
+      @reads << ctlr
+    end
+
+    def looping
+      loop_expire
+
+      loop do
+        rs, ws = IO.select( @reads, @writes )
+
+        @mutex.synchronize do
+          rs.each do | sock |
+            case @roles[ sock ]
+            when :ctlr
+              read_ctlr( sock )
+            when :udpd
+              read_udpd( sock )
+            when :dest
+              read_dest( sock )
+            end
+          end
+
+          ws.each do | sock |
+            close_dest( sock )
+          end
+        end
+      end
+    end
+
+    def quit!
+      exit
+    end
+
+    private
+
+    def read_ctlr( ctlr )
+      dest_id = ctlr.read( 8 ).unpack( 'Q>' ).first
+      dest = @udpd_info[ :dest_ids ][ dest_id ]
+
+      if dest
+        # puts "debug expire dest #{ dest_id } #{ Time.new }"
+
+        unless @writes.include?( dest )
+          @writes << dest
+        end
+      end
+    end
+
+    def read_udpd( udpd )
+      data, addrinfo, rflags, *controls = udpd.recvmsg
+
+      info = @infos[ udpd ]
+      udp_addr = addrinfo.to_sockaddr
+      dest = info[ :udp_addrs ][ udp_addr ]
+
+      unless dest
+        dest_addr = data
+        return unless Addrinfo.new( dest_addr ).ip?
+
+        dest = Socket.new( Socket::AF_INET, Socket::SOCK_DGRAM, 0 )
+        dest.setsockopt( Socket::SOL_SOCKET, Socket::SO_REUSEPORT, 1 )
+        dest.bind( Socket.sockaddr_in( 0, '0.0.0.0' ) )
+        # puts "debug a new dest bound on #{ dest.local_address.ip_unpack.last } #{ Time.new }"
+
+        dest_id = @hex.gen_random_num
+        info[ :udp_addrs ][ udp_addr ] = dest
+        info[ :dest_ids ][ dest_id ] = dest
+        info[ :dests ][ dest ] = {
+          dest_id: dest_id,
+          udp_addr: udp_addr,
+          dest_addr: dest_addr,
+          last_recv_at: Time.new
+        }
+
+        @roles[ dest ] = :dest
+        @reads << dest
+
+        return
+      end
+
+      dest_info = info[ :dests ][ dest ]
+      dest.sendmsg( data, 0, dest_info[ :dest_addr ] )
+    end
+
+    def read_dest( dest )
+      data, addrinfo, rflags, *controls = dest.recvmsg
+
+      dest_info = @udpd_info[ :dests ][ dest ]
+      return unless dest_info
+
+      dest_info[ :last_recv_at ] = Time.new
+      @udpd.sendmsg( data, 0, dest_info[ :udp_addr ] )
+    end
+
+    def close_dest( dest )
+      dest.close
+      @reads.delete( dest )
+      @writes.delete( dest )
+      @roles.delete( dest )
+      dest_info = @udpd_info[ :dests ].delete( dest )
+      @udpd_info[ :udp_addrs ].delete( dest_info[ :udp_addr ] )
+      @udpd_info[ :dest_ids ].delete( dest_info[ :dest_id ] )
+    end
+
+    def loop_expire
+      Thread.new do
+        loop do
+          sleep 60
+
+          @mutex.synchronize do
+            now = Time.new
+
+            @udpd_info[ :dests ].values.each do | dest_info |
+              if now - dest_info[ :last_recv_at ] > 3600
+                @ctlw.write( dest_info[ :dest_id ].pack( 'Q>' ) )
+              end
+            end
+          end
+        end
+      end
+    end
+
+  end
+end
