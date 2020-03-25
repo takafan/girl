7430271c9fed37a070c6ff15ac3c547561f1a0d2
takafan <takafan@163.com>
2020-03-25 12:30:52 +0800

    add udp

--- lib/girl/udpd.rb
+++ lib/girl/udpd.rb
@@ -24,9 +24,9 @@ module Girl
         ctlr => :ctlr, # :ctlr / :udpd / :dest
         udpd => :udpd
       }
-      @dests = {}      # usd_addr => dest
+      @dests = {}      # us_addr => dest
       @dest_infos = {} # dest => {}
-                       #   usd_addr: [ udp_addr, src_addr, dest_addr ].join
+                       #   us_addr: [ udp_addr, src_addr ].join
                        #   udp_addr: udp_addr
                        #   src_addr: src_addr
                        #   last_traff_at: now
@@ -64,12 +64,12 @@ module Girl
     private
 
     def read_ctlr( ctlr )
-      usd_addr = ctlr.read( 48 )
-      dest = @dests[ usd_addr ]
+      us_addr = ctlr.read( 32 )
+      dest = @dests[ us_addr ]
 
       if dest
         unless @writes.include?( dest )
-          # puts "debug expire dest #{ usd_addr.inspect } #{ Time.new }"
+          # puts "debug expire dest #{ us_addr.inspect } #{ Time.new }"
           @writes << dest
         end
       end
@@ -87,9 +87,9 @@ module Girl
       dest_addr = data[ 16, 16 ]
       return unless Addrinfo.new( dest_addr ).ipv4?
 
-      usd_addr = [ udp_addr, src_addr, dest_addr ].join
+      us_addr = [ udp_addr, src_addr ].join
       data = data[ 32..-1 ]
-      dest = @dests[ usd_addr ]
+      dest = @dests[ us_addr ]
 
       unless dest
         dest = Socket.new( Socket::AF_INET, Socket::SOCK_DGRAM, 0 )
@@ -97,9 +97,9 @@ module Girl
         dest.bind( Socket.sockaddr_in( 0, '0.0.0.0' ) )
         # puts "debug a new dest bound on #{ dest.local_address.ip_unpack.last } #{ Time.new }"
 
-        @dests[ usd_addr ] = dest
+        @dests[ us_addr ] = dest
         @dest_infos[ dest ] = {
-          usd_addr: usd_addr,
+          us_addr: us_addr,
           udp_addr: udp_addr,
           src_addr: src_addr,
           last_traff_at: Time.new
@@ -113,7 +113,7 @@ module Girl
         dest.sendmsg( data, 0, dest_addr )
       rescue Errno::EACCES, Errno::EINTR => e
         puts "dest sendmsg #{ e.class } #{ Time.new }"
-        @ctlw.write( usd_addr )
+        @ctlw.write( us_addr )
       end
     end
 
@@ -133,7 +133,7 @@ module Girl
       @writes.delete( dest )
       @roles.delete( dest )
       dest_info = @dest_infos.delete( dest )
-      @dests.delete( dest_info[ :usd_addr ] )
+      @dests.delete( dest_info[ :us_addr ] )
     end
 
     def loop_expire
@@ -146,7 +146,7 @@ module Girl
 
             @dest_infos.values.each do | dest_info |
               if now - dest_info[ :last_traff_at ] > 1800
-                @ctlw.write( dest_info[ :usd_addr ] )
+                @ctlw.write( dest_info[ :us_addr ] )
               end
             end
           end
